784692c29c70558aef0339579a243fc925fbb34d
log1cally <jppc201@exeter.ac.uk>
2026-02-16 22:24:18 +0000

    Update turn delay to 2 seconds

--- Project97/Assets/Scripts/TurnManager.cs
+++ Project97/Assets/Scripts/TurnManager.cs
@@ -32,7 +32,7 @@ public class TurnManager : MonoBehaviour
     private Character computerCharacter;
 
     private bool running;
-    private float moveDelay = 1f;
+    private float moveDelay = 2f;
     private MovesUIScreen movesUIScreen;
     public event Action<bool> RoundComplete; //If player won then call with True, otherwise if player has died we call with false.
     // Start is called once before the first execution of Update after the MonoBehaviour is created
