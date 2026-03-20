4ac9a0b7ff96fe4a047811c60328e4a45de5d368
Monte426 <jtmgmwill5@outlook.com>
2026-03-20 15:39:45 +0000

    Difficulty menu added

--- Project97/Assets/Scripts/UI/StartScreenNav.cs
+++ Project97/Assets/Scripts/UI/StartScreenNav.cs
@@ -9,11 +9,25 @@ public class StartScreenNav : MonoBehaviour
     public GameObject backButton;
     public GameObject settingsButton;
     public GameObject settingsMenu;
+    [SerializeField] private GameObject diffSelectMenu;
     [SerializeField] private GameObject sBackButton;
-    
+    [SerializeField] private GameObject easyButton;
+    [SerializeField] private GameObject normalButton;
+    [SerializeField] private GameObject hardButton;
     public void EnterGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void StartGame()
+    {
+        mainMenu.SetActive(false);
+        diffSelectMenu.SetActive(true);
+        backButton.SetActive(true);
+        settingsMenu.SetActive(false);
+        easyButton.SetActive(true);
+        normalButton.SetActive(true);
+        hardButton.SetActive(true);
     }
 
     public void OpenSettings()
@@ -24,11 +38,6 @@ public class StartScreenNav : MonoBehaviour
     public void CloseSetttings()
     {
         settingsMenu.SetActive(false);
-    }
-
-    public void StartBattle()
-    {
-        SceneManager.LoadScene("Main Scene");
     }
 
     public void ExitGame()
